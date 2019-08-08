1905234a89e6b06afca07686720daa3a8cdd17cd
Brian Ruff <thelonewolf198927@gmail.com>
2019-08-08 15:48:16 -0400

    app-be-sw-master--setup_knex_and_basic_node_server_and_basic_pg -- setup local pg database, init and configure knex to pg, document common procedures with knex and pg database

---
+++ knexSetup.md
@@ -0,0 +1,7 @@
+# Knex Setup MD
+
+1. `npm i -g knex`
+2. Make sure knex is also installed locally
+3. `knex init`
+4. `npm i dotenv` for storing and keeping env variables secret 😯
+5. `require("env").config();` pulls in the .env for the file in question
---
+++ knexfile.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: process.env.CLIENT,
+    connection:'postgres://localhost/<examples>',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/production'
+    },
+    useNullAsDefault: true
+  }
+}
