823eea0713f5e0b7fb2bd2c60bcdda6079b86416
Brian Ruff <thelonewolf198927@gmail.com>
2019-08-17 04:08:33 -0400

    working on authentication routes for /registration. I am having some issues with my POST request to create a new user in the DATABASE, but I have an open ticket on SO, so we will see how that goes. That place is hit or miss, and I fucking hate it when it's a miss because I will have to deal with some fucking ass-hole who thinks they are superior, yet they refuse to answer the question or some BS like that, fuck stack overflow, fuck them up their stupid asses (sometimes that is, no not really, I love you stack overflow, but you crazy as hell though, love you girl).

--- index.js
+++ index.js
@@ -2,26 +2,19 @@ require("dotenv").config();
 
 const express = require("express");
 const server = express();
-const PORT = parseInt(process.env.PORT, 10);
-
-// html template function
-const htmlTemplate = require("./htmlTemplate.js");
 
 // middleware
 const parseMiddleWare = require("./middleWare/middleware.js");
 parseMiddleWare(server);
 
-// routes
-const userRoutes = require("./routes/userRoutes.js");
-server.use("/users", userRoutes);
-
+// Home Page of API
 server.get("/", (req, res) => {
-  res.send(htmlTemplate("<h1>It works</h1>"));
+  res.send("<h1>It works</h1>");
 });
 
-server.listen(PORT, (req, res) => {
+server.listen(process.env.PORT || 3000, (req, res) => {
   console.log('====================================================================='); 
-  const consoleMessage = `Server running on port ${process.env.PORT || PORT}`;
+  const consoleMessage = `Server running on port ${process.env.PORT || 3000}`;
   process.stdout.write(consoleMessage.padStart(50) + "\n");
   console.log('=====================================================================');
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,33 +1,26 @@
 const router = require("express").Router();
 const database = require("../db/pgConfig");
+const errors = require("../ErrorMessages/errorIndex");
 
-// all users route
+// GET ALL USERS LIST
 router.get("/", async (req, res) => {
   const date = new Date();
   const loggableDate = date.toLocaleDateString();
   const loggableTime = date.toLocaleTimeString();
   try {
-    const { rows: users } = await database.query("SELECT * from USERS");
+    const { rows: users } = await database.query(process.env.GET_ALL_USERS);
     if (users.length < 1) {
-      res.statusMessage = "Client side error, AKA the problem is on your computer or a connected device. Please ensure that you typed everything correctly and that are no network problems with your computer. Validate that you don't have a firewall setup to block this website. You can also try clearing the cache and doing a hard reload of the page. Please contact support if this problem continues through our \"Step-by-Step Technical Support Guide\" feature. It's mostly pictures, so it will be fun :)";
-      return res.status(404).json({ statusCode: res.statusCode, message: res.statusMessage, date: loggableDate, time: loggableTime });
+      errors.clientSideError404;
+      return res.status(404).json({ error: error.stack, message: errors.clientSideError404, date: loggableDate, time: loggableTime });
     } else {
       return res.status(200).json({ users });
     }
   } catch (error) {
-    console.error(date, time, "Get All Users Server Error Details >> ", error.stack);
-    return res.status(500).json({date: loggableDate, time: loggableTime, error: error.stack, message: "The problem is on our side. However, there are some things you can try to resolve it without going through the support process. First, try revisiting the page at a later time. It may have been an unknown error that we didn't for-see, but were able to quickly repair it without too much trouble. It's best to always report these errors to the support team via our \"Step-by-Step Technical Support Guide\" feature. It's mostly pictures, so it will be fun :)" });
-
+    return res.status(500).json({ error: error.stack, message: error.serverSideErrorMessage500, date: loggableDate, time: loggableTime });
   }
 });
 
-/*  Page Division Marker
-=====================================================
-=====================================================
-=====================================================
-*/
-
- // single user by id route
+// GET USER by ID
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
   console.log(req.params.id);
@@ -35,17 +28,14 @@ router.get("/:id", async (req, res) => {
   const loggableDate = date.toLocaleDateString();
   const loggableTime = date.toLocaleTimeString();
   try {
-    const user = await database.query("SELECT * from USERS WHERE USERS.id = $1", [id]);
+    const user = await database.query(process.env.GET_SINGLE_USER, [id]);
     if (!user.rows[0]) {
-      // setting message, don't return!!
-      res.statusMessage = "Client side error, AKA the problem is on your computer or a connected device. Please ensure that you typed everything correctly and that are no network problems with your computer. Validate that you don't have a firewall setup to block this website. You can also try clearing the cache and doing a hard reload of the page. Please contact support if this problem continues through our \"Step-by-Step Technical Support Guide\" feature. It's mostly pictures, so it will be fun :)";
-      return res.status(404).json({ statusCode: res.statusCode, message: res.statusMessage, date: loggableDate, time: loggableTime });
+      return res.status(404).json({ statusCode: res.statusCode, message: errors.clientSideError404, date: loggableDate, time: loggableTime });
     } else {
       return res.status(200).json(user.rows[0]);
     }
   } catch (error) {
-    return res.status(500).json({date: loggableDate, time: loggableTime, error: error.stack, message: "The problem is on our side. However, there are some things you can try to resolve it without going through the support process. First, try revisiting the page at a later time. It may have been an unknown error that we didn't for-see, but were able to quickly repair it without too much trouble. It's best to always report these errors to the support team via our \"Step-by-Step Technical Support Guide\" feature. It's mostly pictures, so it will be fun :)" });
-
+    return res.status(500).json({ error: error.stack, message: errors.serverSideErrorMessage500, date: loggableDate, time: loggableTime });
   }
 });
 
