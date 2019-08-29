869b1fd6d1f81c67076acf8e91004163509d1e1d
Brian Ruff <thelonewolf198927@gmail.com>
2019-08-29 14:11:05 -0400

    valid email

--- routes/authenticationRoutes.js
+++ routes/authenticationRoutes.js
@@ -24,6 +24,12 @@ router.post("/register", async (req, res) => {
 
   let { password } = req.body;
 
+  const isValidEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email)
+
+  if (!isValidEmail) {
+    return res.status(403).json({message: "Invalid email"});
+  }
+
   // TODO send proper message when name/email is already in use than what we have now...
 
   // Extra protection from adding Admin Users
