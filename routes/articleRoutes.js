afdcbef18f333f615614edbe0426366d0f80cd65
Brian Ruff <thelonewolf198927@gmail.com>
2019-10-10 19:14:27 -0400

    added in get article by id db get call

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -4,15 +4,17 @@ const formatPGErrors = require("../ErrorMessages/formatPGErrors");
 const protectAdminScope = require("../middleWare/protectRoutesAdminScope");
 const protectMemberScope = require("../middleWare/protectRoutesMemberScope");
 
-
-
 router.get("/", async (req, res) => {
-
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
 
   try {
-    const articles = await database.query("SELECT * FROM ARTICLES ORDER BY id DESC");
+    const articles = await database.query(
+      "SELECT * FROM ARTICLES ORDER BY id DESC"
+    );
     if (!articles.rows[0] || !articles.rows[0].length < 1) {
       return res.status(403).json({ message: "No Articles Found" });
     } else {
@@ -23,26 +25,67 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
-
+router.get("/:id", async (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  
-  const { title, text, author, likes, dislikes, read_time, created_at, updated_at, user_id } = req.body;
-  
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  const { id } = req.params;
+
+  try {
+    const article = await database.query(
+      "SELECT * FROM ARTICLES WHERE ARTICLES.id = $1",
+      [id]
+    );
+
+    if (!article) {
+      return res.status(403).json({ message: "No article" });
+    } else {
+      return res.status(200).json({ article: article.rows[0] });
+    }
+  } catch (error) {
+    return res.status(500).json(formatPGErrors(error));
+  }
+});
+
+router.post("/", async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  const {
+    title,
+    text,
+    author,
+    likes,
+    dislikes,
+    read_time,
+    created_at,
+    updated_at,
+    user_id
+  } = req.body;
+
   if (title.length < 10 || text.length < 50) {
-    return res.status(403).json({message: "Title must be at least 10 characters long, and its content must be at least 50 characters long."});
+    return res.status(403).json({
+      message:
+        "Title must be at least 10 characters long, and its content must be at least 50 characters long."
+    });
   } // quality content via minimum length...
 
   try {
-    await database.query("INSERT INTO ARTICLES (title, text, author, user_id) VALUES ($1, $2, $3, $4)", [
-      title,
-      text,
-      author,
-      user_id
-    ]);
+    await database.query(
+      "INSERT INTO ARTICLES (title, text, author, user_id) VALUES ($1, $2, $3, $4)",
+      [title, text, author, user_id]
+    );
   } catch (error) {
-    return res.status(500).json({error: formatPGErrors(error), message: "Failed to POST new ARTICLE"}); // server error 500
+    return res.status(500).json({
+      error: formatPGErrors(error),
+      message: "Failed to POST new ARTICLE"
+    }); // server error 500
   }
 
   const newArticle = {
@@ -55,10 +98,12 @@ router.post("/", async (req, res) => {
     created_at,
     updated_at,
     user_id
-  }
-  
+  };
+
   if (!newArticle && Object.keys(newArticle).length !== 9) {
-    return res.status(403).json({message: "Failed to POST new Article due to invalid article properties"}); // invalid article
+    return res.status(403).json({
+      message: "Failed to POST new Article due to invalid article properties"
+    }); // invalid article
   } else {
     return res.status(200).json(newArticle); // success
   }
