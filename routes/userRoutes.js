4618017d55cdbe946e94e6a58c4e2767831928d8
brianaruff <brff19@gmail.com>
2021-03-17 20:21:30 -0400

    change header

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,10 +11,6 @@ router.use(cors());
 // GET ALL USERS LIST
 router.get("/", async (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
   const date = new Date();
   const loggableDate = date.toLocaleDateString();
   const loggableTime = date.toLocaleTimeString();
